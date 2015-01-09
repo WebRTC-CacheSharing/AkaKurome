bd7f9c5c51a6dd2a9af45c9cf7c0ea49dcd7520a
Pine Mizune <pinemz@gmail.com>
2015-01-09 17:17:47 +0900

    Refactor

--- Akame/app/js/background.ts
+++ Akame/app/js/background.ts
@@ -1,5 +1,10 @@
 ﻿/// <reference path="../../typings.d.ts" />
 
+// Desc   : サーバーの起動処理と、ヘルパー拡張機能との通信処理
+// License: MIT License
+// Author : pine613<https://github.com/pine613>
+// Copyright (C) 2014-2015 Pine Mizune.
+
 import _ = require('underscore');
 
 import global = require('../../../global');
@@ -18,16 +23,16 @@ var hostInfo: global.HostInfoMessage = {
 };
     
 /**
-    * サーバーを開始する
-    */
+ * サーバーを開始する
+ */
 function startServer(): void {
     httpServer = new server.HttpServer();
     httpServer.listen('0.0.0.0', hostInfo.port);
 }
 
 /**
-    * アプリケーションを開始する
-    */
+ * アプリケーションを開始する
+ */
 function startApp(lanchData: chrome.app.runtime.LaunchData): void {
     chrome.app.window.create('/app/html/app.html', {
         bounds: {
@@ -35,8 +40,6 @@ function startApp(lanchData: chrome.app.runtime.LaunchData): void {
             height: 600
         }
     }, win => {
-        console.log(win);
-
             win.contentWindow.addEventListener('DOMContentLoaded', () => {
 
                 var appWin = <app.AppContentWindow>win.contentWindow;
@@ -44,29 +47,28 @@ function startApp(lanchData: chrome.app.runtime.LaunchData): void {
                     httpServer: httpServer
                 };
 
-                console.log(appWin.startApp);
                 appWin.startApp(appArgs);
             });
         });
 }
 
 /**
-    * ホスト情報をヘルパー拡張機能へ送信
-    */
+ * ホスト情報をヘルパー拡張機能へ送信
+ */
 function sendHostInfo(): void {
     chrome.runtime.sendMessage(global.HELPER_EXTENSION_ID, hostInfo);
 }
 
 /**
-    * 他の拡張機能からメッセージを受け取った時の処理
-    */
+ * 他の拡張機能からメッセージを受け取った時の処理
+ */
 function onMessageExternal(
     message: global.Message,
     sender: chrome.runtime.MessageSender,
     sendResponse: (data: any) => void): void {
 
     if (message.type == 'keepAlive') {
-        var keepAlive = <global.KeepAliveMessage>(message);
+        var keepAlive = <global.KeepAliveMessage>message;
             
         // ホスト情報を要求されている場合
         if (keepAlive.needHostInfo) {
@@ -77,14 +79,17 @@ function onMessageExternal(
 }
 
 /**
-    * イベントを追加する
-    */
+ * イベントを追加する
+ */
 function addEvents(): void {
     console.log('::addEvents');
     chrome.runtime.onMessageExternal.addListener(onMessageExternal);
     chrome.app.runtime.onLaunched.addListener(startApp);
 }
 
+/**
+ * サーバーの起動ポートを乱数で決定する
+ */
 function getServerPort(): number {
     return 11000 + Math.floor(Math.random() * 100);
 }
